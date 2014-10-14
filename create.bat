e51683caa2cbab62076046809ad2d16f467bea30
gothandy <gothandy@hotmail.com>
2014-10-14 18:45:33 +0100

    Include Web.config in Project file.
    Changes to include web.config but not override one supplied by Sitecore.

--- create.bat
+++ create.bat
@@ -3,6 +3,7 @@ del * /F /Q /S
 copy ..\Website\Website.sln Website.sln
 copy ..\Website\Website\Website.csproj Website.csproj
 copy ..\Website\Website\packages.config packages.config
+copy ..\Website\Website\web.*.config
 mkdir Views
 copy ..\Website\Website\Views Views
 mkdir Properties
