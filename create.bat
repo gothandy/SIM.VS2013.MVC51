dea164319bf1f90863cf62adc58b5ad8f5711856
gothandy <gothandy@hotmail.com>
2014-10-14 12:51:13 +0100

    Fixed problem with ZIP
    Took out the 5.1 just incase. Noticed a problem with the ZIP file format
    and added a batch file. Also fixed some issues with the project
    definition.

---
+++ create.bat
@@ -0,0 +1,10 @@
+cd "Visual Studio 2013 MVC 51 Website Project"
+del * /F /Q /S
+copy ..\Website\Website.sln Website.sln
+copy ..\Website\Website\Website.csproj Website.csproj
+copy ..\Website\Website\packages.config packages.config
+mkdir Views
+copy ..\Website\Website\Views Views
+mkdir Properties
+copy ..\Website\Website\Properties Properties
+cd ..
